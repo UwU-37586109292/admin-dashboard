bddb1ec884d707c59ce3ea6043b966d5a11b4c4a
Zuzka <kikucisko@gmail.com>
2022-09-03 10:33:50 +0200

    Add icons to cards

---
+++ script.js
@@ -0,0 +1,22 @@
+const cards = document.getElementsByClassName('card');
+
+for (let i = 0; i < cards.length; i++) {
+    const iconsContainer = document.createElement('div');
+    iconsContainer.setAttribute('class', 'icons');
+
+    const favIcon = document.createElement('img');
+    favIcon.setAttribute('src', './assets/star-plus-outline.png');
+    const observeIcon = document.createElement('img');
+    observeIcon.setAttribute('src', './assets/eye-plus-outline.png');
+
+    const forkIcon = document.createElement('img');
+    forkIcon.setAttribute('src', './assets/source-fork.png');
+    iconsContainer.appendChild(favIcon);
+    iconsContainer.appendChild(observeIcon);
+    iconsContainer.appendChild(forkIcon);
+
+    const element = cards[i];
+    element.appendChild(iconsContainer);
+}
+
+// cards[1].appendChild(favIcon);
